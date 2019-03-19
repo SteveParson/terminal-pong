5592aabe91be7986c12a7e37fe12dda9575bbd2c
Steve Parson <k47swp@mun.ca>
2019-03-18 23:51:09 -0400

    style: minor code changes

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # terminal-pong
-Pong in your terminal!
+
+Pong - in your terminal!
+
+## Getting Started
+
+Run `make`, then `./main`
+
+### Prerequisites
+
+A C compiler and ncurses development libraries
+
+### Installing
+
+For maximum fun, install it as your shell.
+`chsh $USER /path/to/main` (not recommended)
+
+## Built With
+
+* [ncurses](https://www.gnu.org/software/ncurses/) - The curses emulator
+* [gcc](https://gcc.gnu.org/) - The GNU compiler collection
+
+## Contributing
+
+Would someone please make this a 2 player game?
+
+## License
+
+[Apache License version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+## Acknowledgments
+
+Thanks for checking it out
--- main.c
+++ main.c
@@ -11,129 +11,141 @@ int WINSIZE_CHANGE = 0;
 int PADDLE_SIZE = 6;
 
 // Window resize callback
-void winResize(int dummyvar)
-{
+void winResize(int dummyvar) {
+    // Set flag to indicate window size changed
+    WINSIZE_CHANGE = 1;
 
-	// Set flag to indicate window size changed
-	WINSIZE_CHANGE = 1;
-
-	// Update terminal data so that getmaxyx uses the
-	// right data
-	struct winsize size;
-	if (ioctl(fileno(stdout), TIOCGWINSZ, &size) == 0) {
-		resize_term(size.ws_row, size.ws_col);
-	}
-
+    // Update terminal data so that getmaxyx uses the
+    // right data
+    struct winsize size;
+    if(ioctl(fileno(stdout), TIOCGWINSZ, &size) == 0) {
+        resize_term(size.ws_row, size.ws_col);
+    }
 }
 
 // Draw the paddle
-void drawPaddle(int ypos, int xpos)
-{
-
-	for (int i = 0; i <= PADDLE_SIZE; i++)
-		mvprintw(ypos + i, xpos, "*");
-
+void drawPaddle(int ypos, int xpos) {
+    for(int i = 0; i <= PADDLE_SIZE; i++) {
+        mvprintw(ypos + i, xpos, "*");
+    }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    // position, screen boundaries, direction constants
+    int x     = 11, y     = 11,
+        maxX  = 0,  maxY = 0,
+        nextX = 0,  nextY = 0,
+        dirX  = 1,  dirY  = 1,
+        padX  = 10, padY  = 10;
+    
+    double relposbX = 0,
+           relpospY = 0,
+           relposbY = 0;
+    
+    char kbinput;
 
-	// position, screen boundaries, direction constants
-	int x = 11, y = 11, maxX = 0, maxY = 0, nextX = 0,
-	    nextY = 0, dirX = 1, dirY = 1, padX = 10, padY = 10;
-	double relposbX = 0, relposbY = 0, relpospY = 0;
-	char kbinput;
+    // Initialize screen
+    // Use non-blocking getch()
+    WINDOW *w = initscr();
+    cbreak();
+    nodelay(w, TRUE);
+    noecho();
+    curs_set(FALSE);
 
-	// Initialize screen
-	// Use non-blocking getch()
-	WINDOW *w = initscr();
-	cbreak();
-	nodelay(w, TRUE);
-	noecho();
-	curs_set(FALSE);
+    // Get max Y and X values for terminal
+    getmaxyx(stdscr, maxY, maxX);
 
-	// Get max Y and X values for terminal
-	getmaxyx(stdscr, maxY, maxX);
+    // Set up signal handler in case user resizes terminal
+    signal(SIGWINCH, winResize);
 
-	// Set up signal handler in case user resizes terminal
-	signal(SIGWINCH, winResize);
+    while(true) {
+        // Our window size has changed
+        // Recalculate relative positions
+        if(WINSIZE_CHANGE) {
+            usleep(1500000);
+            getmaxyx(stdscr, maxY, maxX);
+            
+            x    = round(maxX * relposbX);
+            y    = round(maxY * relposbY);
+            padY = round(maxY * relpospY);
+            
+            WINSIZE_CHANGE = 0;
+        } else {
+            // Wait, then redraw at new position
+            usleep(50000);
+            clear();
+            printw("[ and ] to move paddle.  Q to quit\n");
 
-	while (true) {
+            mvprintw(y, x, "O");
+            kbinput = getch();
 
-		// Our window size has changed
-		// Recalculate relative positions
-		if (WINSIZE_CHANGE) {
-			usleep(1500000);
-			getmaxyx(stdscr, maxY, maxX);
-			x = round(maxX * relposbX);
-			y = round(maxY * relposbY);
-			padY = round(maxY * relpospY);
-			WINSIZE_CHANGE = 0;
-		} else {
-			// Wait, then redraw at new position
-			usleep(50000);
-			clear();
-			printw("[ and ] to move paddle.  Q to quit\n");
-			//printw("Window Size Y=%d X=%d\n", maxY, maxX);
-			//printw("Position Y=%d X=%d\n", y, x);
-			mvprintw(y, x, "O");
-			kbinput = getch();
+            // Very basic keyboard handling
+            // Use '['  and   ']' to move paddle
+            if(kbinput != ERR) {
+                
+                if(kbinput == '[' && padY >= 2) {
+                    padY -= 2;
+                }
+                
+                if(kbinput == ']'
+                   && padY < (maxY - PADDLE_SIZE - 2)) {
+                    
+                    padY += 2;
+                }
+                
+                if(kbinput == 'q' || kbinput == 'Q') {
+                    endwin();
+                    printf("Thanks for playing!\n");
+                    return 0;
+                }
+            }
 
-			// Very basic keyboard handling
-			// Use '['  and   ']' to move paddle
-			if (kbinput != ERR) {
+            drawPaddle(padY, padX);
+            refresh();
 
-				if (kbinput == '[' && padY >= 2)
-					padY -= 2;
-				if (kbinput == ']' &&
-				    padY < (maxY - PADDLE_SIZE - 2))
-					padY += 2;
-				if (kbinput == 'q' || kbinput == 'Q') {
-					endwin();
-					printf("Thanks for playing!\n");
-					return 0;
-				}
+            // Handle bounce off of the walls
+            nextX = x + dirX;
+            nextY = y + dirY;
 
-			}
+            // Very basic paddle bounce
+            if(nextX == 10
+               && dirX == -1
+               && padY <= nextY
+               && (padY + PADDLE_SIZE) >= nextY) {
+                dirX *= -1;
+            }
+            
+            if(nextX == 9
+               && dirX == 1
+               && padY <= nextY
+               && (padY + PADDLE_SIZE) >= nextY) {
+                    dirX *= -1;
+            }
 
-			drawPaddle(padY, padX);
-			refresh();
+            // Hit X boundary, change direction
+            if(nextX >= maxX || nextX < 0) {
+                dirX *= -1;
+            }
+            else {
+                x += dirX;
+            }
 
-			// Handle bounce off of the walls
-			nextX = x + dirX;
-			nextY = y + dirY;
+            // Hit Y boundary, change direction
+            if(nextY >= maxY || nextY < 0) {
+                dirY *= -1;
+            }
+            else {
+                y += dirY;
+            }
 
-			// Very basic paddle bounce
-			if (nextX == 10 && dirX == -1 && padY <= nextY &&
-			    padY + PADDLE_SIZE >= nextY)
-				dirX *= -1;
-			if (nextX == 9 && dirX == 1 && padY <= nextY &&
-			    padY + PADDLE_SIZE >= nextY)
-				dirX *= -1;
+            if(maxX > 0 && maxY > 0) {
+                relposbX = (double) x / maxX;
+                relposbY = (double) y / maxY;
+                relpospY = (double) padY / maxY;
+            }
+        }
+    }
 
-
-			// Hit X boundary, change direction
-			if (nextX >= maxX || nextX < 0)
-				dirX *= -1;
-			else
-				x += dirX;
-
-			// Hit Y boundary, change direction
-			if (nextY >= maxY || nextY < 0)
-				dirY *= -1;
-			else
-				y += dirY;
-
-			if (maxX > 0 && maxY > 0) {
-				relposbX = (double)x / maxX;
-				relposbY = (double)y / maxY;
-				relpospY = (double)padY / maxY;
-			}
-		}
-
-	}
-
-	endwin();
-	return 0;
-
+    endwin();
+    return 0;
 }
